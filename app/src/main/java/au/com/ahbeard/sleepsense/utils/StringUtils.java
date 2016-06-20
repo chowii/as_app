80f3fd5d9e593827edf40ae6db97611240fb54bf
Neal Maloney <neal.maloney@shrinkray.com.au>
2016-06-20 16:04:02 +1000

    GitHub issue #53.

--- app/src/main/java/au/com/ahbeard/sleepsense/utils/StringUtils.java
+++ app/src/main/java/au/com/ahbeard/sleepsense/utils/StringUtils.java
@@ -75,16 +75,19 @@ public class StringUtils {
     }
 
     public static String ordinal(int integer) {
-        String[] suffixes = new String[]{"th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"};
-        switch (integer % 100) {
-            case 11:
-            case 12:
-            case 13:
-                return integer + "th";
-            default:
-                return integer + suffixes[integer % 10];
 
-        }
+        return integer + "";
+
+//        String[] suffixes = new String[]{"th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"};
+//        switch (integer % 100) {
+//            case 11:
+//            case 12:
+//            case 13:
+//                return integer + "th";
+//            default:
+//                return integer + suffixes[integer % 10];
+//
+//        }
     }
 
 }
