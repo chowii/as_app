a1841582acef99624b61b118dddb49094dbcd256
Luis Ramos <luis@mentallyfriendly.com>
2016-06-29 18:08:02 +1000

    Add zeds animation to sleep now screen

--- app/src/main/java/au/com/ahbeard/sleepsense/activities/SleepTrackingActivity.java
+++ app/src/main/java/au/com/ahbeard/sleepsense/activities/SleepTrackingActivity.java
@@ -1,13 +1,33 @@
 package au.com.ahbeard.sleepsense.activities;
 
+import android.animation.Animator;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import au.com.ahbeard.sleepsense.R;
@@ -65,6 +85,8 @@ public class SleepTrackingActivity extends AppCompatActivity {
     @Bind(R.id.sleep_tracking_layout_connecting)
     View mConnectingLayout;
 
+    int spawnerCounter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -102,6 +124,11 @@ public class SleepTrackingActivity extends AppCompatActivity {
                     @Override
                     public void call(Integer packetCount) {
                         mSampleCountTextView.setText(Integer.toString(packetCount));
+
+                        if (spawnerCounter % 2 == 0) {
+                            spawnZed();
+                        }
+                        spawnerCounter++;
                     }
                 });
 
@@ -111,6 +138,9 @@ public class SleepTrackingActivity extends AppCompatActivity {
             SleepSenseDeviceService.instance().getPumpDevice().connectToGetFirmness();
         }
 
+        for (int i = 0; i < 20; i++){ //prefill zed pool
+            zedsPool.add(createZed());
+        }
     }
 
     private void setButtonState() {
@@ -169,5 +199,91 @@ public class SleepTrackingActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Bind(R.id.zeds_container)
+    FrameLayout zedsContainer;
+    Long animationDuration = 5500L;
+    ArrayList<ImageView> zedsPool = new ArrayList<>();
+    Random r = new Random();
+
+    private void spawnZed() {
+        final ImageView zed = zedsPool.size() > 0 ? zedsPool.remove(zedsPool.size() - 1) : createZed();
+
+        final int width = zedsContainer.getWidth();
+        final int height = zedsContainer.getHeight();
+
+        float spawnWidth = dpToPx(50);
+        float goalWidth = dpToPx(70);
+
+        int spawnMargin = Math.round(spawnWidth / 2);
+        float fromXDelta = spawnMargin - r.nextInt(Math.round(spawnWidth));
+        float toXDelta = r.nextInt(Math.round(goalWidth)) * (fromXDelta > 0 ? 1 : -1);
+        float toYDelta = dpToPx(-100);
+
+        float angle = Math.round(Math.toDegrees(-Math.atan(-toYDelta / (toXDelta - fromXDelta))));
+
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) zed.getLayoutParams();
+        layoutParams.leftMargin = (width - zed.getWidth()) / 2 + Math.round(fromXDelta);
+        layoutParams.topMargin = height - zed.getHeight();
+        zed.setLayoutParams(layoutParams);
+        zed.setVisibility(View.VISIBLE);
+
+        AnimationSet animationSet = new AnimationSet(false);
+
+        TranslateAnimation moveAnim = new TranslateAnimation(fromXDelta, toXDelta, 0, toYDelta);
+        moveAnim.setInterpolator(new DecelerateInterpolator());
+        moveAnim.setDuration(animationDuration);
+        moveAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                recycleZed(zed);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        animationSet.addAnimation(moveAnim);
+
+        ScaleAnimation scaleAnim = new ScaleAnimation(0, 1.5f, 0, 1.5f);
+        scaleAnim.setInterpolator(new DecelerateInterpolator());
+        scaleAnim.setDuration(animationDuration);
+        animationSet.addAnimation(scaleAnim);
+
+        AlphaAnimation alphaAnim = new AlphaAnimation(1, 0);
+        alphaAnim.setDuration(animationDuration);
+        animationSet.addAnimation(alphaAnim);
+
+//        RotateAnimation rotateAnimation = new RotateAnimation(0, angle, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+//        rotateAnimation.setDuration(animationDuration);
+//        animationSet.addAnimation(rotateAnimation);
+
+        zed.startAnimation(animationSet);
+    }
+
+    private void recycleZed(ImageView zed) {
+        zed.setVisibility(View.INVISIBLE);
+        zedsPool.add(zed);
+    }
+
+    private ImageView createZed() {
+        ImageView imageView = new ImageView(this);
+        imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.zed_icon));
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        imageView.setLayoutParams(layoutParams);
+        imageView.setVisibility(View.INVISIBLE);
+        zedsContainer.addView(imageView);
+        return imageView;
+    }
+
+    private float dpToPx(float dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
     }
 }
