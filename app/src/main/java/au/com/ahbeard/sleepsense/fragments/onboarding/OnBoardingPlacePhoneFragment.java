1e99c5c3ab447de17b2173aba2d920395e778720
Luis Ramos <luis@mentallyfriendly.com>
2016-06-30 12:54:18 +1000

    Add animation to PlacePhoneOnBed screen

--- app/src/main/java/au/com/ahbeard/sleepsense/fragments/onboarding/OnBoardingPlacePhoneFragment.java
+++ app/src/main/java/au/com/ahbeard/sleepsense/fragments/onboarding/OnBoardingPlacePhoneFragment.java
@@ -3,12 +3,18 @@ package au.com.ahbeard.sleepsense.fragments.onboarding;
 import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import au.com.ahbeard.sleepsense.R;
 import au.com.ahbeard.sleepsense.services.AnalyticsService;
@@ -79,9 +85,11 @@ public class OnBoardingPlacePhoneFragment  extends OnBoardingFragment{
             mPlacePhoneBedImageView.setImageResource(R.drawable.onboarding_background_place_phone_right);
         }
 
-        if ( mPhoneImageView.getDrawable() instanceof AnimationDrawable ) {
-            ((AnimationDrawable)mPhoneImageView.getDrawable()).start();
-        }
+//        if ( mPhoneImageView.getDrawable() instanceof AnimationDrawable ) {
+//            ((AnimationDrawable)mPhoneImageView.getDrawable()).start();
+//        }
+
+        startPulseAnimation();
 
         return view;
     }
@@ -114,5 +122,39 @@ public class OnBoardingPlacePhoneFragment  extends OnBoardingFragment{
 
     public interface OnActionListener {
         void onPlacePhoneContinueClicked();
+    }
+
+    @Bind({R.id.pulse_1, R.id.pulse_2, R.id.pulse_3})
+    List<ImageView> pulses;
+
+    private void startPulseAnimation() {
+        long animationDelay = 0;
+        for (final ImageView imageView : pulses) {
+            // Delay each animation by 200ms
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Animation pulse = AnimationUtils.loadAnimation(getContext(), R.anim.pulse);
+                    pulse.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+                            imageView.setVisibility(View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+                    imageView.startAnimation(pulse);
+                }
+            }, animationDelay);
+            animationDelay += 400;
+        }
     }
 }
